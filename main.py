a506f23662f859e385a5921e1d23a1ab09780e54
ataniki <165153764+ataniki@users.noreply.github.com>
2024-05-22 11:42:03 +0000

    testando replic

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def estimar_tempo_viagem(distância, velocidade_média):
   tempo = distância/velocidade_média
   retorn tempo
 
-distância = 200
+distância =200
 velocidade_media = 60
 tempo_estimado =estimar_tempo_viagem(distância, velocidade_ media)
 print ("para uma viagem de {}km e velocidade média de {} km, o tempo estimado é de {}". format ( distancia, velocidade_média, tempo_estimado))
