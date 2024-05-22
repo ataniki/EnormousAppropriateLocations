c22f3dcd499e67424e5e8e91876b792a6ed0e8c9
00001114235441s <33637135-00001114235441s@users.noreply.replit.com>
2024-05-22 11:38:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def estimar_tempo_viagem(distância, velocidade_média): 
+  tempo = distância/velocidade_média
+  retorn tempo
+
+distância = 200
+velocidade_media = 60
+tempo_estimado =estimar_tempo_viagem(distância, velocidade_ media)
+print ("para uma viagem de {}km e velocidade média de {} km, o tempo estimado é de {}". format ( distancia, velocidade_média, tempo_estimado))
+
